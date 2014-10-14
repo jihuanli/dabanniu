ede1f9e3b45188952c89906f5cc39b1a3283c527
jihuanli <jihuanli@dabanniu.com>
2014-10-14 19:04:43 +0800

    add

--- .openshift/cron/minutely/auto_pull.sh
+++ .openshift/cron/minutely/auto_pull.sh
@@ -2,7 +2,8 @@
 log_file="$OPENSHIFT_DATA_DIR/auto_pull.txt"
 exec 2>&1 1>> $log_file &
 cd $OPENSHIFT_DEPLOYMENTS_DIR/201*/repo/spider/baiduzhidao/baiduzhidao/spiders
-PIDS=`ps -ef |grep scrapy |grep -v grep | awk '{print $2}'`
-#if [ ~"$PIDS" ]; then
-#  scrapy crawl zhidao >$OPENSHIFT_DATA_DIR/log.txt 2>&1 &
-#fi
+PID_CNT=`ps -ef |grep scrapy |grep -v grep | wc -l`
+if [ $PID_CNT != 1 ]; then
+  ps -ef | grep scrapy | grep zhidao | awk '{print $2}' | xargs -n 1 -r kill -9
+  scrapy crawl zhidao >$OPENSHIFT_DATA_DIR/log.txt 2>&1 &
+fi
