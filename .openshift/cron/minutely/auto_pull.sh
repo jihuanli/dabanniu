a81a071fbb0388b4261db5e119dc2fa609e69a09
jihuanli <jihuanli@dabanniu.com>
2014-10-16 16:20:25 +0800

    auto-commit

--- .openshift/cron/minutely/auto_pull.sh
+++ .openshift/cron/minutely/auto_pull.sh
@@ -4,6 +4,6 @@ exec 2>&1 1>> $log_file &
 cd $OPENSHIFT_DEPLOYMENTS_DIR/201*/repo/spider/baiduzhidao/baiduzhidao/spiders
 PID_CNT=`ps -ef |grep scrapy |grep zhidao |grep -v grep | wc -l`
 if [ $PID_CNT != 1 ]; then
-  ps -ef | grep scrapy | grep zhidao | awk '{print $2}' | xargs -n 1 -r kill -9
+  ps -ef | grep scrapy | grep zhidao |grep -v grep | awk '{print $2}' | xargs -n 1 -r kill -9
   scrapy crawl zhidao >$OPENSHIFT_DATA_DIR/zhidao_log.txt 2>&1 &
 fi
--- spider/baiduzhidao/baiduzhidao/pipelines.py
+++ spider/baiduzhidao/baiduzhidao/pipelines.py
@@ -11,10 +11,16 @@ from scrapy import log
 from baiduzhidao.items import ZhidaoQuestion,ZhidaoAnswer,RelatedQuestion,QuestionPic,AnswerPic, QuestionViewNum,RelatedTopic
 
 #get md5 of a input string 
-def GetStringMD5(str):
+def GetStringMD5(to_md5_str):
     m = md5.new()
-    m.update(str)
+    m.update(to_md5_str)
     return m.hexdigest()
+def TransformSQLString(sql_str):
+    if sql_str == None:
+        return ""
+    sql_str = sql_str.replace("\n", "\\n");
+    sql_str = sql_str.replace("'", "\'")  
+    return sql_str
 
 class BaiduzhidaoPipeline(object):
     def __init__(self): 
@@ -43,8 +49,6 @@ class BaiduzhidaoPipeline(object):
         result_file_name = str(spider.result_filename_prefix) + str(item.get('product_id')) + "_" + str(item.get('task_id')) + "_" + str(timestamp) + "_" + str(spider.result_filenname_suffix)
         result_file = open(result_file_name, 'a')
         result_sql = result_sql.encode("utf-8")
-        result_sql = result_sql.replace("\n", "\\n")
-        result_sql = result_sql.replace("'", "\"")
         result_sql = result_sql.replace("None", "null")
         result_sql = result_sql + "\n"
         result_file.write(result_sql)
@@ -56,32 +60,32 @@ class BaiduzhidaoPipeline(object):
     def _question_insert(self, item):
         if item.get('questionId'):
             md5_str = GetStringMD5(str(item['questionId']) + str(item['product_id']))
-            return "insert into question(productId, questionId, url, title, content, supplyContent, category, userName, time,keyword, isFinish, md5) values(%s, %s, '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', %s, '%s');" % (item['product_id'], item['questionId'], item['url'], item['title'], item['content'], item['supplyContent'], item['category'], item['userName'], item['time'], item['keyword'], item['isFinish'], md5_str)
+            return "insert into question(productId, questionId, url, title, content, supplyContent, category, userName, time,keyword, isFinish, md5) values(%s, %s, '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', %s, '%s');" % (item['product_id'], item['questionId'], TransformSQLString(item['url']), TransformSQLString(item['title']), TransformSQLString(item['content']), TransformSQLString(item['supplyContent']), TransformSQLString(item['category']), TransformSQLString(item['userName']), TransformSQLString(item['time']), TransformSQLString(item['keyword']), item['isFinish'], md5_str)
 
     def _answer_insert(self, item):
         if item.get('questionId'):
             md5_str = GetStringMD5(str(item['questionId']) + str(item['answerId']))
-            return "insert into answer (answerId, questionId, likeNum, content, userName, time, isBest, md5) values(%s, %s, %s, '%s', '%s', '%s', %s, '%s');" % (item['answerId'], item['questionId'], item['likeNum'], item['content'], item['userName'], item['time'], item['isBest'], md5_str)
+            return "insert into answer (answerId, questionId, likeNum, content, userName, time, isBest, md5) values(%s, %s, %s, '%s', '%s', '%s', %s, '%s');" % (item['answerId'], item['questionId'], item['likeNum'], TransformSQLString(item['content']), TransformSQLString(item['userName']), TransformSQLString(item['time']), item['isBest'], md5_str)
 
     def _related_question_insert(self, item):
         if item.get('questionId'):
             md5_str = GetStringMD5(str(item['questionId']) + str(item['relatedId']))
-            return "insert into relatedQuestion (relatedId, questionId, likeNum, title, time, md5) values(%s, %s, %s, '%s', '%s', '%s');" % (item['relatedId'], item['questionId'], item['likeNum'], item['title'], item['time'], md5_str)
+            return "insert into relatedQuestion (relatedId, questionId, likeNum, title, time, md5) values(%s, %s, %s, '%s', '%s', '%s');" % (item['relatedId'], item['questionId'], item['likeNum'], TransformSQLString(item['title']), TransformSQLString(item['time']), md5_str)
 
     def _related_topic_insert(self, item):
         if item.get('questionId'):
             md5_str = GetStringMD5(str(item['questionId']) + str(item['relatedId']))
-            return "insert into relatedTopic (relatedId, questionId, likeNum, title, time, md5) values(%s, %s, %s, '%s', '%s', '%s');" % (item['relatedId'], item['questionId'], item['likeNum'], item['title'], item['time'], md5_str)
+            return "insert into relatedTopic (relatedId, questionId, likeNum, title, time, md5) values(%s, %s, %s, '%s', '%s', '%s');" % (item['relatedId'], item['questionId'], item['likeNum'], TransformSQLString(item['title']), TransformSQLString(item['time']), md5_str)
 
     def _question_pic_insert(self, item):
         if item.get('questionId'):
             md5_str = GetStringMD5(str(item['questionId']) + str(item['picUrl']))
-            return "insert into questionPic (questionId, picUrl, md5) values(%s, '%s', '%s');" % (item['questionId'], item['picUrl'], md5_str)
+            return "insert into questionPic (questionId, picUrl, md5) values(%s, '%s', '%s');" % (item['questionId'], TransformSQLString(item['picUrl']), md5_str)
 
     def _answer_pic_insert(self, item):
         if item.get('answerId'):
             md5_str = GetStringMD5(str(item['answerId']) + str(item['picUrl']))
-            return "insert into answerPic (answerId, picUrl, md5) values(%s, '%s', '%s');" % (item['answerId'], item['picUrl'], md5_str)
+            return "insert into answerPic (answerId, picUrl, md5) values(%s, '%s', '%s');" % (item['answerId'], TransformSQLString(item['picUrl']), md5_str)
 
     def _question_view_update(self, item):
         if item.get('questionId'):
