817d71cfa169c84e99f1b89716be88def412f136
jihuanli <jihuanli@dabanniu.com>
2014-10-21 09:44:52 +0800

    auto-commit

--- .openshift/cron/minutely/zhidao_tar_files.sh
+++ .openshift/cron/minutely/zhidao_tar_files.sh
@@ -4,13 +4,16 @@
 # up again.  This script gets executed directly, so it could be python, php,
 # ruby, etc.
 # 每天凌晨0-1点取一个小时前的数据文件生成tar包
-log_file="$OPENSHIFT_DATA_DIR/zhidao_tar.log"
+spider_name="zhidao"
+log_file="$OPENSHIFT_DATA_DIR/$spider_name""_tar.log"
+echo $log_file
 exec 2>&1 1>> $log_file &
 minutes_ago=60
-date_scp="$(date +%Y%m%d)06"
+#时差12个小时
+date_scp="$(date +%Y%m%d)12"
 date_h=$(date +%Y%m%d%H)
 date=$(date +%Y%m%d)
-dest_dir="$OPENSHIFT_DATA_DIR/zhidao"
+dest_dir="$OPENSHIFT_DATA_DIR/$spider_name"
 result_tar_filename="$date.tar.gz"
 
 if [ $date_h -eq $date_scp ];then
