84424b0af5f1c67eee70ff9d02003d2fa6a5beb1
jihuanli <jihuanli@dabanniu.com>
2014-10-22 21:48:45 +0800

    auto-commit

--- push_code_to_spiders.sh
+++ push_code_to_spiders.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git add .
 git commit -m "auto-commit"
-git push spider1 && git push spider2 && git push spider3 && git push spider4 && git push spider5 && git push spider6 && git push spider7 && git push spider8 && git push spider9 && git push spider10 && git push spider11 &&git push spider12
+git push spider3 && git push spider4 && git push spider5 && git push spider6 && git push spider7 && git push spider8 && git push spider9 && git push spider10 && git push spider12
