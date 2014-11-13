16b1346660e10c255bfaaa78f6268bf10d6c3798
root <root@localhost.(none)>
2014-11-13 11:35:22 +0800

    modify

--- push_code_to_spiders.sh
+++ push_code_to_spiders.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git add .
 git commit -m "auto-commit"
-git push spider1 && git push spider2 && git push spider3 && git push spider4 && git push spider5 && git push spider6 && git push spider7 && git push spider8 && git push spider9 && git push spider10 && git push spider11 && git push spider12
+ git push spider1 && git push spider2 && git push spider3 && git push spider4 && git push spider5 && git push spider6 && git push spider7 && git push spider8 && git push spider9 && git push spider10 && git push spider11 && git push spider12 && git push spider13 && git push spider14 && git push spider15 && git push spider16 && git push spider17 && git push spider18 && git push spider19 && git push spider20 && git push spider21 && git push spider22 && git push spider23 && git push spider24
