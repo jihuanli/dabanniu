d7318619897083347fc14c9c8bffdc303d773028
root <root@iZ25ywls4e4Z.(none)>
2015-03-13 12:40:55 +0800

    add

--- spider/tmall/tmall/pipelines.py
+++ spider/tmall/tmall/pipelines.py
@@ -9,17 +9,23 @@ from scrapy.log import err
 import time
 import md5
 def GetStringMD5(to_md5_str):
+    err("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&")
+    err("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&")
     m = md5.new()
     m.update(to_md5_str)
     return m.hexdigest()
 
 class TmallPipeline(object):
     def process_item(self, item, spider):
+        err("============================")
         if isinstance(item,ProductCommonItem):
+            err("%%%%%%%%%%%%%%%%%%%%%%%%%%%")
             result_sql = self.common(item)  
         if isinstance(item,ProductImgItem):
+            err("***************************")
             result_sql = self.img(item) 
         if isinstance(item,ProductDetailItem):
+            err("^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
             result_sql = self.detail(item)
         timestamp=time.strftime('%Y-%m-%d',time.localtime(time.time()))
         result_file_name=str(spider.result_filename_prefix)+str(item['productId'])+"_"+str(item['taskId'])+"_"+str(timestamp)+"_"+str(spider.result_filename_suffix)
@@ -32,15 +38,18 @@ class TmallPipeline(object):
         return item
          
     def common(self,item):
+        err("1111111111111111111111111111")
         sql = "insert into tmall(productId,brand,name,send_address,url,catId,parameter,description)values(%s,'%s','%s','%s','%s',%s,'%s','%s')" % (long(item['productId']),item['brand'],item['name'],item['send_address'],item['url'],int(item['catId']),item['parameter'],item['description'])
         return sql 
 
     def img(self,item):
+        err("2222222222222222222222222222")
         md5_str = GetStringMD5(str(item['productId'])+item['brand_big_img'])
         sql =  "insert into tmall_img(productId,brand_big_img,brand_little_img,md5)values(%s,'%s','%s','%s')" % (long(item['productId']),item['brand_big_img'],item['brand_little_img'],md5_str)
         return sql
      
     def detail(self,item):
+        err("33333333333333333333333333333")
         md5_str=GetStringMD5(str(item['productId'])+item['color_name']+item['standard'])
         sql =  "insert into tmall_detail(productId,origin_price,standard,skuId,color_big_img,color_little_img,color_name,stock,md5)values(%s,%s,'%s',%s,'%s','%s','%s',%s,'%s')" % (long(item['productId']),float(item['origin_price']),item['standard'],item['skuId'],item['color_big_img'],item['color_little_img'],item['color_name'],item['stock'],md5_str)
         return sql
