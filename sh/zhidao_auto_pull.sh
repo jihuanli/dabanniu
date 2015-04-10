91d8e109ce136f7b666ed8349cc37161d9372eff
root <root@iZ25ywls4e4Z.(none)>
2015-04-11 00:59:51 +0800

    auto

--- sh/zhidao_auto_pull.sh
+++ sh/zhidao_auto_pull.sh
@@ -5,6 +5,6 @@ exec 2>&1 1>> $log_file &
 cd $OPENSHIFT_DEPLOYMENTS_DIR/201*/repo/spider/baiduzhidao/baiduzhidao/spiders
 PID_CNT=`ps -ef |grep scrapy |grep zhidao |grep -v grep | wc -l`
 if [ $PID_CNT != 1 ]; then
-  ps -ef | grep scrapy | grep zhidao |grep -v grep | awk '{print $2}' | xargs -n 1 -r kill -9
+#  ps -ef | grep scrapy | grep zhidao |grep -v grep | awk '{print $2}' | xargs -n 1 -r kill -9
   scrapy crawl zhidao >"$OPENSHIFT_DATA_DIR/$spider_name""_log.txt" 2>&1 &
 fi
