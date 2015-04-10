46959e2f488f0a7b8c5fdcd88ef99484f899ce03
root <root@iZ25ywls4e4Z.(none)>
2015-04-10 23:13:43 +0800

    auto-commit

--- push_code_to_spiders.sh
+++ push_code_to_spiders.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 git add .
 git commit -m "auto-commit"
-git push spider1 && git push spider2 && git push spider3 && git push spider4 && git push spider5 && git push spider6 && git push spider7 && git push spider8 && git push spider9 && git push spider10 && git push spider11 && git push spider12 && git push spider13 && git push spider14 && git push spider15 && git push spider16 && git push spider17 && git push spider18 && git push spider19 && git push spider20 && git push spider21 && git push spider22 && git push spider23 && git push spider24 && git push spider25 && git push spider26 && git push spider27 && git push spider28 && git push spider29 && git push spider30
-
+git push spider1 &
+set spider_base_name = "spider"
+for ((i=1;i<31;i++));do
+{
+  set spider_name = ${spider_base_name}${i} 
+  echo $spider_name
+  git push $spider_name 
+}&
+done
+wait
